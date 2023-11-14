cb75adfdb85f3d745dc4ce504a1ceceb562d4608
Andrew Lanning <116489166+andrewlanning@users.noreply.github.com>
2023-11-14 17:22:39 -0600

    Update main.py
    small variable and function updates

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ import base64
 
 username = ''
 password = ''
-customerID = ''
+customer_id = ''
 
 s = requests.Session() #stores your cookies, yum
 
 def encode_pw(username, password):
-    initialHash = hashlib.sha256((password + username.lower()).encode('utf-8')).digest()
-    hashInBase64 = base64.b64encode(initialHash).decode('utf-8')
-    return hashInBase64
+    initial_hash = hashlib.sha256((password + username.lower()).encode('utf-8')).digest()
+    hash_base64 = base64.b64encode(initial_hash).decode('utf-8')
+    return hash_base64
 
-pwValueToSubmit = encode_pw(username, password)
+pw_submit = encode_pw(username, password)
 
 def auth():   
-    authInfo = {'email': username, 'password': pwValueToSubmit}
-    s.post('https://members-ng.iracing.com/auth', data=authInfo)
+    auth_info = {'email': username, 'password': pw_submit}
+    s.post('https://members-ng.iracing.com/auth', data=auth_info)
    
-def myInfo():
-    cID = {'cust_id':customerID}
-    r = s.get('https://members-ng.iracing.com/data/member/info', params=cID).json()
+def my_info():
+    cust_id = {'cust_id':customer_id}
+    r = s.get('https://members-ng.iracing.com/data/member/info', params=cust_id).json()
     r = s.get(r["link"]).json()
     
     print("First Name:", r["first_name"])
@@ -29,13 +29,13 @@ def myInfo():
     print("Member Since:", r["member_since"])
     print("Club:", r["club_name"])
 
-def winSummary():
-    cID = {'cust_id':customerID}
-    r = s.get('https://members-ng.iracing.com/data/stats/member_summary', params=cID).json()
+def win_summary():
+    cust_id = {'cust_id':customer_id}
+    r = s.get('https://members-ng.iracing.com/data/stats/member_summary', params=cust_id).json()
     r = s.get(r["link"]).json()
     
     print("Wins this year:", r['this_year']['num_official_wins'])
 
 auth()
-myInfo()
-winSummary()
+my_info()
+win_summary()
